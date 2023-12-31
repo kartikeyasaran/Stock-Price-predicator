eff9c27e6a5665f848dc6d056bf4d489b139d7e5
Kartikey Saran <kartiksaran07@gmail.com>
2023-12-31 17:55:30 +0000

    Development phase 02

--- Main.py
+++ Main.py
@@ -36,3 +36,53 @@ x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 #model dev
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1)) #Predicting the next closing price
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+'''Testing the Model Accuracy on existing Prices'''
+
+#Load Test Data
+test_start = dt.datetime(2023,12,29)
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(stock, 'yahoo', test_start, test_end)
+actual_prices= test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+
+model_inputs = total_dataset[len(total_dataset)-len(test_data)-prediction_days:].values
+model_inputs = model_inputs.reshape(-1,1)
+model_inputs = scaler.transform(model_inputs)
+
+#Make prediction on test data
+x_test = []
+
+for x in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[x-prediction_days:x,0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0],x_test.shape[1], 1))
+
+predicted_prices = model.predict(x_test)
+predicted_prices = scaler.inverse_transform(predicted_prices)
+
+#plot the Test Predictions
+plt.plot(actual_prices, color='black', label=f'Actual {stock} Price')
+plt.plot(predicted_prices, color='green', label=f'Predicted {stock} Price')
+plt.title(f'{stock} Share Price')
+plt.xlabel('Time')
+plt.ylabel('PRICE')
+plt.legend()
+plt.show()
+
+#Predict The Next Day
